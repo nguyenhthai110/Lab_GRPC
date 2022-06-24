6ba9d07a0bfaf43f49edcaeea3b4d18b85435bbb
thainh2 <thainh2@bagroup.vn>
2022-06-24 10:28:49 +0700

    first commit

---
+++ GrpcClient/GrpcClient/GrpcClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using Grpc.Net.Client;
+using GrpcClient.Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("press any key to cont ...");
+            Console.ReadLine();
+
+            using (var channel = GrpcChannel.ForAddress("https://localhost:5001"))
+            {
+                var client = new Greeter.GreeterClient(channel);
+
+                for (int i = 0; i < 9999999; i++)
+                {
+                    var mes = new MessageData()
+                    {
+                        ID = i,
+                        MesContent = "thainh_" + i,
+                        Note = string.Empty
+                    };
+
+                    var idx = 0;
+
+                    while (idx < 10)
+                    {
+                        try
+                        {
+                            var reply = await client.SayHelloAsync(new HelloRequest { Name = "thainh_" + i });
+                            Console.WriteLine($"Name: {reply.Message}");
+                            await Task.Delay(1000);
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            idx++;
+                            await Task.Delay(1000);
+
+                        }
+
+                    }
+
+                }
+
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
---
+++ GrpcService/GrpcService/GrpcService/Entities/MessageData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrpcClient.Entities
+{
+    public class MessageData
+    {
+        public int ID { get; set; }
+        public string MesContent { get; set; }
+        public string Note { get; set; }
+    }
+}
